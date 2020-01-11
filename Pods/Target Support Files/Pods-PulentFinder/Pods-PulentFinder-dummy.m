6649fa8ba767c0b6fc646b862f6fee7fd397a3ce
Gaspar Andres Valenzuela Gonzalez <gvalenzuela@technisys.com>
2020-01-11 19:58:17 -0300

    - Se instala cocoapods para integrar hasta el momento Alamofire, el cual se usara para las peticiones hacia la API de itunes. - Se crea la Clase SearchAlbumService que hace la petición a la API de itunes.

---
+++ Pods/Target Support Files/Pods-PulentFinder/Pods-PulentFinder-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PulentFinder : NSObject
+@end
+@implementation PodsDummy_Pods_PulentFinder
+@end
---
+++ PulentFinder/Services/SearchAlbumService.swift
@@ -0,0 +1,37 @@
+//
+//  SearchAlbumService.swift
+//  PulentFinder
+//
+//  Created by Gaspar Valenzuela on 11-01-20.
+//  Copyright © 2020 Gaspar Valenzuela. All rights reserved.
+//
+
+import Foundation
+
+import Alamofire
+
+
+class SearchAlbumService{
+    
+    public static func searchAlbumWithName(withName name: String?, withPage page:String?, _ completion: @escaping (_ success: Bool, _ result: Array<Any>?, _ error: Error?) -> ()){
+    
+        let linkPart1 = "https://itunes.apple.com/search?term=in+"
+        let linkPart2 = "&mediaType=music&limit="
+        
+        var url:String =  linkPart1 + (name ?? "")
+         url = url + linkPart2 + (page ?? "")
+        
+        Alamofire.request(url).responseJSON
+        { response in switch response.result {
+          
+          case .success(let JSON):
+            
+             let results = ((JSON as! NSDictionary)["results"]) as? Array<Any> ?? Array()
+             completion(true, results, nil)
+             
+            case .failure(let error):
+                completion(false, [] as Array<Any>, error)
+         }
+        }
+    }
+}
