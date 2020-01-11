f4458366129fe8d44a48e79c0ac9836f58346a58
Gaspar Andres Valenzuela Gonzalez <gvalenzuela@technisys.com>
2020-01-11 19:30:28 -0300

    initial commit

---
+++ PulentFinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PulentFinder
+//
+//  Created by Gaspar Valenzuela on 11-01-20.
+//  Copyright © 2020 Gaspar Valenzuela. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
