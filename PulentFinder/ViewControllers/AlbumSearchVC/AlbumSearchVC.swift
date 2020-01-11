b18de6c0ae37ec61a4326e1b395297f4fcbd5f8a
Gaspar Andres Valenzuela Gonzalez <gvalenzuela@technisys.com>
2020-01-11 19:40:36 -0300

    - Se quita el mainStoryboard y se deja el AlbumSearchVC como primer controlador

---
+++ PulentFinder/ViewControllers/AlbumSearchVC/AlbumSearchVC.swift
@@ -0,0 +1,18 @@
+//
+//  AlbumSearchVC.swift
+//  PulentFinder
+//
+//  Created by Gaspar Valenzuela on 11-01-20.
+//  Copyright © 2020 Gaspar Valenzuela. All rights reserved.
+//
+
+import UIKit
+
+class AlbumSearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
